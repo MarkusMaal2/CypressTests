d6986d262a5c24f7058ca39c2521c83cc08757fc
MarkusMaal2 <markus.maal@voco.ee>
2023-10-31 09:17:25 +0200

    Generalized the accept cookies test to all languages

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -4,13 +4,15 @@ describe('Accept cookies', () => {
 		cy.visit('https://google.com')
 		// check if google.com is in the URL
 		cy.url().should("include", "google.com")
+		// check if title is Google
+		cy.title("Google")
 	})
 	it("Click \"Nõustu kõigiga\"", () => {
 		cy.visit('https://google.com')
-		// click "Accept all"
-		cy.get("button").contains("Nõustu kõigiga").click()
+		// click the last sibling of first button (Accept all)
+		cy.get("button").siblings().last().click()
 		// check if "Accept all" button is invisible
-		cy.get("button").contains("Nõustu kõigiga").should("not.be.visible")
+		cy.get("button").siblings().last().should("not.be.visible")
 	})
 })
 
