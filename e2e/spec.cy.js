25b0be96390b0b0c8ae18b955546493fd32b6e3c
MarkusMaal2 <markus.maal@voco.ee>
2023-10-31 09:52:25 +0200

    Click first result test

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -4,8 +4,6 @@
 
 // function to accept cookies without typing these command every time
 Cypress.Commands.add("acceptCookies", () => {
-	// visit Google
-	cy.visit('https://google.com')
 	// click the last sibling of first button (Accept all)
 	cy.get("button").siblings().last().click()
 })
@@ -20,6 +18,8 @@ describe('Accept cookies', () => {
 		cy.title("Google")
 	})
 	it("Click \"Accept all\"", () => {
+		// visit google.com
+		cy.visit('https://google.com')
 		// run accept cookies function
 		cy.acceptCookies()
 		// check if "Accept all" button is invisible
@@ -58,6 +58,27 @@ describe("Regular search", () => {
 		cy.go('back')
 		// let's make sure the title is Google again
 		cy.title().should('equal', 'Google')
+	})
+	it('Click first result', () => {
+		// visit google.com
+		cy.visit('https://google.com')
+		// accept cookis
+		cy.acceptCookies()
+		// type search term to first text area
+		cy.get("textarea").first().type("Cypress E2E testing")
+		// check if the textarea has the correct value
+		cy.get("textarea").first().should("have.value", "Cypress E2E testing")
+		// wait a bit to avoid multi-line input
+		cy.wait(500)
+		// press ENTER key to search
+		cy.get("textarea").first().type("{enter}")
+		// let's make sure the title isn't just Google anymore and contains the search term
+		cy.title().should('not.equal', 'Google')
+		cy.title().should('include', 'Cypress E2E testing')
+		// let's find the first result
+		cy.get("#search").children().first().children().eq(1).children().first().children().first().children().first().children().first().children().first().children().first().children().first().children().first().click()
+		// let's make sure we're not on Google
+		cy.title().should('not.include', 'Google')
 	})
 })
 
