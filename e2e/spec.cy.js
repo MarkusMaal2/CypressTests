829e40037276e747947721d61585f5360a26a826
MarkusMaal2 <markus.maal@voco.ee>
2023-10-31 09:11:21 +0200

    Add accept cookies tests

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -1,17 +1,15 @@
-describe('Visit Google', () => {
-  it('Open Google.com', () => {
+describe('Accept cookies', () => {
+  it('Visit google.com', () => {
+	// visit google.com
     cy.visit('https://google.com')
+	// check if google.com is in the URL
+	cy.url().should("include", "google.com")
   })
-})
-
-describe('Sample Test', () => {
-	it('Does not do much', () => {
-		expect(true).to.equal(true);
-	})
-})
-
-describe('Failing Test', () => {
-	it('Does not do much', () => {
-		expect(true).to.equal(false);
+	it("Click \"Nõustu kõigiga\"", () => {
+		cy.visit('https://google.com')
+		// click "Accept all"
+		cy.get("button").contains("Nõustu kõigiga").click()
+		// check if "Accept all" button is invisible
+		cy.get("button").contains("Nõustu kõigiga").should("not.be.visible")
 	})
 })
