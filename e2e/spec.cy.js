e758cd5dc9a0faf27834fb67b6c518862c5db128
MarkusMaal2 <markus.maal@voco.ee>
2023-10-31 09:25:24 +0200

    Type to search field test

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -1,3 +1,10 @@
+Cypress.Commands.add("acceptCookies", () => {
+	// visit Google
+	cy.visit('https://google.com')
+	// click the last sibling of first button (Accept all)
+	cy.get("button").siblings().last().click()
+})
+
 describe('Accept cookies', () => {
 	it('Visit google.com', () => {
 		// visit google.com
@@ -7,12 +14,22 @@ describe('Accept cookies', () => {
 		// check if title is Google
 		cy.title("Google")
 	})
-	it("Click \"Nõustu kõigiga\"", () => {
-		cy.visit('https://google.com')
-		// click the last sibling of first button (Accept all)
-		cy.get("button").siblings().last().click()
+	it("Click \"Accept all\"", () => {
+		// run accept cookies function
+		cy.acceptCookies()
 		// check if "Accept all" button is invisible
 		cy.get("button").siblings().last().should("not.be.visible")
+	})
+})
+
+describe("Regular search", () => {
+	it('Type to search field', () => {
+		// accept cookis
+		cy.acceptCookies()
+		// type search term to first text area
+		cy.get("textarea").first().type("Cypress E2E testing")
+		// check if the textarea has the correct value
+		cy.get("textarea").first().should("have.value", "Cypress E2E testing")
 	})
 })
 
